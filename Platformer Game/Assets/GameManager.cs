12e939dc12113ce2ff5cdfc5709f9dd7b63d6bf6
Emerald263 <emeraldminer263@gmail.com>
2024-11-24 21:32:49 -0500

    mechanics+textures

--- Platformer Game/Assets/GameManager.cs
+++ Platformer Game/Assets/GameManager.cs
@@ -1,20 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public int score;
-    public TMP_Text scoreText;
-
-
 
     // Start is called before the first frame update
     void Start()
     {
-
-
 
     }
 
@@ -22,18 +16,5 @@ public class GameManager : MonoBehaviour
     void Update()
     {
 
-        scoreText.text = "Score: " + score;
     }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag.Equals("Player"))
-        {
-            Debug.Log("ive been collected!");
-            Destroy(this.gameObject); //destroy the coin
-            score++;
-
-        }
-    }
-
 }
